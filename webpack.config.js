2baf5444c3ea10cd95ae769a49ebeab9fb4d4a5e
Jian-Hua, He <game09116@gmail.com>
2017-10-04 15:18:46 +0800

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,34 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: './src/app.html',
+    filename: 'app.html',
+    inject: 'body',
+});
 
 module.exports = {
-    rules: [
-        {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-        },
-    ],
+    entry: './src/app.js',
+
+    output: {
+        filename: 'app.bundle.js',
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+        ],
+    },
+
+    plugins: [HtmlWebpackPluginConfig],
 };
 
