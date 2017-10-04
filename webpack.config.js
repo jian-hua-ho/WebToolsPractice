6e4843f5f06bb5a95a5237851617c4ac1e57ab25
Jian-Hua, He <game09116@gmail.com>
2017-10-04 12:37:04 +0800

    Set up babel and webpack

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+    rules: [
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+        },
+    ],
+};
+
