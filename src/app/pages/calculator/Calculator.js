493fe0665f3e45e5759ec60824e9a93f54e1b318
Jian-Hua, He <game09116@gmail.com>
2017-11-04 14:30:37 +0800

    Use key press event to replace on change event

--- src/app/pages/calculator/Calculator.js
+++ src/app/pages/calculator/Calculator.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 
 // Components
 import NumberInput from 'app/components/form/number/NumberInput';
@@ -9,29 +10,63 @@ class Calculator extends Component {
     constructor() {
         super();
 
-
+        this.state = {
+            num: 0,
+        };
     }
 
     // Event Handlers
-    _handleBtnClick(str) {
-        alert(str);
+    _handleNumberKeyPress(e) {
+        let oriNumString = _.toString(this.state.num);
+        let keyCode = e.keyCode || e.which;
+        let appendString = String.fromCharCode(keyCode);
+        let value = oriNumString === '0' ? _.toNumber(appendString) : _.toNumber(appendString + oriNumString);
+
+        this.setState({
+            num: value,
+        });
+    }
+
+    _handleEqualClick() {
+        alert('Equal');
+    }
+
+    _handlePlusClick() {
+        alert('Plus');
+    }
+
+    _handleMinusClick() {
+        alert('Minus');
+    }
+
+    _handleBtnClick(num) {
+        alert(num);
     }
 
     // Render
     render() {
         return (
             <div>
-                <NumberInput />
-                <Button onClick={this._handleBtnClick.bind(this, '1')} >1</Button>
-                <Button onClick={this._handleBtnClick.bind(this, '2')} >2</Button>
-                <Button onClick={this._handleBtnClick.bind(this, '3')} >3</Button>
-                <Button onClick={this._handleBtnClick.bind(this, '4')} >4</Button>
-                <Button onClick={this._handleBtnClick.bind(this, '5')} >5</Button>
-                <Button onClick={this._handleBtnClick.bind(this, '6')} >6</Button>
-                <Button onClick={this._handleBtnClick.bind(this, '7')} >7</Button>
-                <Button onClick={this._handleBtnClick.bind(this, '8')} >8</Button>
-                <Button onClick={this._handleBtnClick.bind(this, '9')} >9</Button>
-                <Button onClick={this._handleBtnClick.bind(this, '0')} >0</Button>
+                <div>
+                    <NumberInput
+                        value={this.state.num}
+                        onKeyPress={this._handleNumberKeyPress.bind(this)} />
+                </div>
+                <div>
+                    <Button onClick={this._handleBtnClick.bind(this, 1)} >1</Button>
+                    <Button onClick={this._handleBtnClick.bind(this, 2)} >2</Button>
+                    <Button onClick={this._handleBtnClick.bind(this, 3)} >3</Button>
+                    <Button onClick={this._handleBtnClick.bind(this, 4)} >4</Button>
+                    <Button onClick={this._handleBtnClick.bind(this, 5)} >5</Button>
+                    <Button onClick={this._handleBtnClick.bind(this, 6)} >6</Button>
+                    <Button onClick={this._handleBtnClick.bind(this, 7)} >7</Button>
+                    <Button onClick={this._handleBtnClick.bind(this, 8)} >8</Button>
+                    <Button onClick={this._handleBtnClick.bind(this, 9)} >9</Button>
+                    <Button onClick={this._handleBtnClick.bind(this, 0)} >0</Button>
+                    <Button onClick={this._handleEqualClick.bind(this)} >=</Button>
+                    <Button onClick={this._handlePlusClick.bind(this)} >+</Button>
+                    <Button onClick={this._handleMinusClick.bind(this)} >-</Button>
+                </div>
             </div>
         );
     }
