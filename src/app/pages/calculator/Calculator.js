2d11f71b55e6613e8b29c5167f93649c44bb5038
Jian-Hua, He <game09116@gmail.com>
2017-11-04 11:49:40 +0800

    Add event for each buttons

--- src/app/pages/calculator/Calculator.js
+++ src/app/pages/calculator/Calculator.js
@@ -5,20 +5,26 @@ import NumberInput from 'app/components/form/number/NumberInput';
 import Button from 'app/components/element/button/Button';
 
 class Calculator extends Component {
+    // Event handlers
+    _handleBtnClick(str) {
+        alert(str);
+    }
+
+    // Render
     render() {
         return (
             <div>
                 <NumberInput />
-                <Button>1</Button>
-                <Button>2</Button>
-                <Button>3</Button>
-                <Button>4</Button>
-                <Button>5</Button>
-                <Button>6</Button>
-                <Button>7</Button>
-                <Button>8</Button>
-                <Button>9</Button>
-                <Button>0</Button>
+                <Button onClick={this._handleBtnClick.bind(this, '1')} >1</Button>
+                <Button onClick={this._handleBtnClick.bind(this, '2')} >2</Button>
+                <Button onClick={this._handleBtnClick.bind(this, '3')} >3</Button>
+                <Button onClick={this._handleBtnClick.bind(this, '4')} >4</Button>
+                <Button onClick={this._handleBtnClick.bind(this, '5')} >5</Button>
+                <Button onClick={this._handleBtnClick.bind(this, '6')} >6</Button>
+                <Button onClick={this._handleBtnClick.bind(this, '7')} >7</Button>
+                <Button onClick={this._handleBtnClick.bind(this, '8')} >8</Button>
+                <Button onClick={this._handleBtnClick.bind(this, '9')} >9</Button>
+                <Button onClick={this._handleBtnClick.bind(this, '0')} >0</Button>
             </div>
         );
     }
