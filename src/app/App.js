0b909d207f3706292aef0f8c150c6c1445108aa1
Jian-Hua, He <game09116@gmail.com>
2017-11-04 11:33:28 +0800

    Add calculator page

--- src/app/App.js
+++ src/app/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 // Components
 import Root from 'app/pages/root/Root';
+import Calculator from 'app/pages/calculator/Calculator';
 
 class App extends Component {
     render() {
@@ -10,6 +11,7 @@ class App extends Component {
             <BrowserRouter>
                 <Switch>
                     <Route exact path="/" component={Root} />
+                    <Route path="/calculator" component={Calculator} />
                 </Switch>
             </BrowserRouter>
         );
---
+++ src/app/pages/calculator/Calculator.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Calculator extends Component {
+    render() {
+        return <div>This is calculator</div>
+    }
+}
+
+export default Calculator;
