55df74fca44622937b0038c284895ee1d0d68643
Jian-Hua, He <game09116@gmail.com>
2017-11-04 11:29:00 +0800

    Add Root page

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+// Components
+import Root from 'app/pages/root/Root';
 
 class App extends Component {
     render() {
         return (
-            <div>Hello World!!!</div>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/" component={Root} />
+                </Switch>
+            </BrowserRouter>
         );
     }
 }
