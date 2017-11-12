be6aeb9eb58e2711319e5b92b37fa6690acb9aa8
Jian-Hua, He <game09116@gmail.com>
2017-11-12 12:21:06 +0800

    Add keyCode constants

---
+++ src/app/constants/keyCode.js
@@ -0,0 +1,12 @@
+export const KEYCODE_NUM = {
+    ZERO: 48,
+    ONE: 49,
+    TWO: 50,
+    THREE: 51,
+    FOUR: 52,
+    FIVE: 53,
+    SIX: 54,
+    SEVEN: 55,
+    EIGHT: 56,
+    NINE: 57,
+};
