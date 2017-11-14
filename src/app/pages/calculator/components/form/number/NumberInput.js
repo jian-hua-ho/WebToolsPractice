76ad3f1cf80ebf2626fdb0b09cdf862b6703c113
Jian-Hua, He <game09116@gmail.com>
2017-11-14 23:00:49 +0800

    Add max and min prop

--- src/app/pages/calculator/components/form/number/NumberInput.js
+++ src/app/pages/calculator/components/form/number/NumberInput.js
@@ -2,11 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
 
-const NumberInput = ({ value, onKeyDown, placeholder }) => {
+const NumberInput = ({ value, onKeyDown, max, min, placeholder }) => {
     return (
         <input
             type="number"
             value={value}
+            max={max}
+            min={min}
             onKeyDown={onKeyDown}
             placeholder={placeholder} />
     );
@@ -16,10 +18,14 @@ NumberInput.propTypes = {
     value: PropTypes.number,
     onKeyDown: PropTypes.func,
     placeholder: PropTypes.string,
+    max: PropTypes.number,
+    min: PropTypes.number,
 };
 
 NumberInput.defaultProps = {
     value: 0,
+    min: -99999999,
+    max: 99999999,
 };
 
 export default NumberInput;
