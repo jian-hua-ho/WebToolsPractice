832d96caf7dc55d37f4e55e3fd6cab30582b1eff
Jian-Hua, He <game09116@gmail.com>
2017-12-05 22:32:35 +0800

    Add RandomNum

---
+++ src/app/pages/randomNum/RandomNum.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class RandomNum extends Component {
+    // Render
+    render() {
+        return (
+            <div>Random Num</div>
+        );
+    }
+}
+
+export default RandomNum;
