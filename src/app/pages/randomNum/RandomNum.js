a4c1b85cde9b289089a9c9c9e81d561b94c1be6e
Jian-Hua, He <game09116@gmail.com>
2017-12-09 10:55:53 +0800

    Add style and view

--- src/app/pages/randomNum/RandomNum.js
+++ src/app/pages/randomNum/RandomNum.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 
+import styles from './styles';
+
 class RandomNum extends Component {
     // Render
     render() {
         return (
-            <div>Random Num</div>
+            <div className={styles.randomNumContainer}>
+                <div className={styles.containerCenter}>
+                    <span className={styles.numDisplay}>0</span>
+                </div>
+                <div className={styles.containerCenter}>
+                    <input
+                        className={styles.input}
+                        type="text"
+                        value="1"
+                        placeholder="MIN" />
+                </div>
+                <div className={styles.containerCenter}>
+                    <input
+                        className={styles.input}
+                        type="text"
+                        value="20"
+                        placeholder="MAX" />
+                </div>
+                <div className={styles.containerCenter}>
+                    <button className={styles.btn}>START</button>
+                </div>
+            </div>
         );
     }
 }
