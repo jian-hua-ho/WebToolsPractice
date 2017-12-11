bd8c086d1200fce067f6546a6d9f2268d728f89a
Jian-Hua, He <game09116@gmail.com>
2017-12-11 13:47:18 +0800

    Add MultiLang component

---
+++ src/app/pages/multiLang/MultiLang.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class MultiLang extends Component {
+    // Render
+    render() {
+        return (
+            <div>
+                MultiLang
+            </div>
+        );
+    }
+}
+
+export default MultiLang;
