c984497db8c7eb6f31589c1e3c9dd1b923e1db57
Jian-Hua, He <game09116@gmail.com>
2017-12-16 14:54:07 +0800

    Update rules

--- src/app/pages/validation/services/rules.js
+++ src/app/pages/validation/services/rules.js
@@ -1,5 +1,24 @@
-export const email = (value) => {
-    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const validations = {
+    email: (value) => {
+        let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    return re.test(value);
+        let validator = {
+            isValid: true,
+            message: '',
+        };
+
+        let valid = re.test(value);
+        if (!valid) {
+            validator.isValid = valid;
+            validator.message = 'Email is not valid';
+        }
+
+        return validator;
+    },
+};
+
+export const ruleNames = Object.keys(validations);
+
+export const validate = (rule, value) => {
+    return validations[rule](value);
 };
